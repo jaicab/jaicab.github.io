3a54fb04bcdefe9098fae9d783bfc08cd5b6b71c
Jaime Caballero <lamatesoft@gmail.com>
2016-04-02 11:01:02 +0100

    Timeout for HTML request in Service Worker

---
+++ _posts/2016-04-12-everything-you-do-is-design.md
@@ -0,0 +1,4 @@
+
+
+
+In a flock of fish, there are no leaders. No fish is the leader, and at the same time all of them are. Each fish follows a set of rules and then just keeps swimming. 
--- serviceworker.js
+++ serviceworker.js
@@ -5,6 +5,27 @@ const staticCacheName = version + 'static';
 const pagesCacheName = version + 'pages';
 const imagesCacheName = version + 'images';
 
+
+function timeout(delay) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          resolve(new Response('', {
+              status: 408,
+              statusText: 'Request timed out.'
+          }));
+        }, delay);
+    });
+}
+
+self.addEventListener('fetch', function(event) {
+    // Add 1s timeout to HTML requests
+    if (event.request.headers.get('Accept').indexOf('text/html') !== -1) {
+      event.respondWith(Promise.race([timeout(1000), fetch(event.request.url)]);
+    } else {
+      event.respondWith(fetch(event.request));
+    }
+});
+
 function updateStaticCache() {
 	return caches.open(staticCacheName)
         .then( cache => {
