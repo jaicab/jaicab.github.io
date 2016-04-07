716a9784bd0e4b9822a6f30a309760d7976e8980
Jaime Caballero <lamatesoft@gmail.com>
2016-04-07 20:13:23 +0100

    Fix postCSS, add initial style guide

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,8 +65,7 @@ module.exports = function(grunt) {
         map: true, // inline sourcemaps
         processors: [
           require('pixrem')(), // add fallbacks for rem units
-          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-          require('cssnano')() // minify the result
+          require('autoprefixer')({browsers: 'last 2 versions'}) // add vendor prefixes
         ]
       },
       dist: {
--- _posts/2016-04-12-everything-you-do-is-design.md
+++ _posts/2016-04-12-everything-you-do-is-design.md
@@ -1,4 +1,6 @@
-
+---
+title: Everything you do is design
+---
 
 
 In a flock of fish, there are no leaders. No fish is the leader, and at the same time all of them are. Each fish follows a set of rules and then just keeps swimming. 
